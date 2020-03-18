4dd14034c9452c3436b733e5fc9e1c1f2fdf753c
صفوان القليطي <safwanalqulayti@gmail.com>
2020-03-18 04:00:46 +0300

    test

--- config/db.js
+++ config/db.js
@@ -6,9 +6,9 @@ const mongooseBaseName = 'courseExam'
 // Create the MongoDB URI for Development and Test
 const database = {
   development: `mongodb://localhost/${mongooseBaseName}`,
-  test: `mongodb://safwanheroku:Ss0560054277@ds159371.mlab.com:59371/heroku_49v13xht`
+  test: `mongodb://safwanheroku:Ss0560054277@ds041623.mlab.com:41623/heroku_drs167t3`
 }
-
+ 
 // Identify if development environment is Test or Development
 // select DB based on whether a test file was executed before `server.js`
 const localDB = process.env.TESTENV ? database.test : database.development
