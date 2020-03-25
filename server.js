8af34a35dac8bc8cbad777210b60124341fa8e03
صفوان القليطي <safwanalqulayti@gmail.com>
2020-03-25 08:11:07 +0300

    connect the atlas

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors= require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT ||7000
@@ -12,11 +11,14 @@ const userRoute = require('./routes/users')
 const imageRoute = require('./routes/image')
 const  path = require("path");
 
-
+app.use(cors())
+app.use(express.json());
 app.use('/courses' , coursesRoute)
 app.use('/users' , userRoute)
  
 app.use('/image' , imageRoute)
+require('dotenv').config();
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
@@ -26,9 +28,10 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
-app.use(express.json());
+
+
 
 const uri = process.env.ATLAS_URI;
   mongoose.connect(uri , {useNewUrlParser: true , useCreateIndex: true});
