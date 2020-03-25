8fc6df959d067d9d3ab42536c63c91aa8a9dd388
صفوان القليطي <safwanalqulayti@gmail.com>
2020-03-25 07:52:36 +0300

    connect the atlas

--- server.js
+++ server.js
@@ -12,16 +12,6 @@ const userRoute = require('./routes/users')
 const imageRoute = require('./routes/image')
 const  path = require("path");
 
-app.use(cors())
-app.use(express.json());
-
-const uri = process.env.ATLAS_URI;
-  mongoose.connect(uri , {useNewUrlParser: true , useCreateIndex: true});
-  const connection = mongoose.connection
-  connection.once("open",()=>{
-    console.log("Mongo db connection is working ")
-  })
-
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
@@ -32,6 +22,17 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+  mongoose.connect(uri , {useNewUrlParser: true , useCreateIndex: true});
+  const connection = mongoose.connection
+  connection.once("open",()=>{
+    console.log("Mongo db connection is working ")
+  })
+
+
  
 // const db = require("./config/db")
  
